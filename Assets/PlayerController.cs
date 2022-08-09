42eb9a9f2988831eb07437b9ca522722d00e072d
Kirpitchnsk <98724575+Kirpitchnsk@users.noreply.github.com>
2022-08-09 23:23:43 +0700

    Changes

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 touchpadInput;
+    public Transform cameratransform;
+    private CapsuleCollider capsuleCollider;
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+    private void FixedUptade()
+    {
+        Vector3 movementDir = Player.instance.hmdTransform.TransformDirection(new Vector3(touchpadInput.axis.x, 0, touchpadInput.axis.y));
+        transform.position += (Vector3.ProjectOnPlane(Time.deltaTime * movementDir * 2.0f, Vector3.up));
+        float distanceFromFloor = Vector3.Dot(cameratransform.localPosition, Vector3.up);
+        capsuleCollider.height = Mathf.Max(capsuleCollider.radius, distanceFromFloor);
+
+        capsuleCollider.center = cameratransform.localPosition - 0.5f * distanceFromFloor * Vector3.up;
+    }
+}
